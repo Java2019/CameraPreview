91273e448b8ce8f9f54c7b1ea9e6703c52afc038
java2019 <java_2019@mail.ru>
2017-07-02 20:14:23 +0300

    test1

--- app/src/main/java/com/samples/camera/preview/CameraPreviewActivity.java
+++ app/src/main/java/com/samples/camera/preview/CameraPreviewActivity.java
@@ -1,13 +1,53 @@
 package com.samples.camera.preview;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
-public class CameraPreviewActivity extends AppCompatActivity {
+public class CameraPreviewActivity extends AppCompatActivity
+        implements SurfaceHolder.Callback{
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder holder;
+
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_preview);
+
+        surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
+        holder = surfaceView.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            // Открываем камеру
+            camera = Camera.open();
+            camera.setPreviewDisplay(holder);
+            // Запускаем просмотр
+            camera.startPreview();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
     }
 }
