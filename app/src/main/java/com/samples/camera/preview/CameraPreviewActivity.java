99729bc24d2f1588e338781509f788e3323375a9
java2019 <java_2019@mail.ru>
2017-07-02 20:09:11 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/camera/preview/CameraPreviewActivity.java
@@ -0,0 +1,13 @@
+package com.samples.camera.preview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_preview);
+    }
+}
